50df352b4b08cadf45c7cf29a346f133a4ef26e7
Aaron <ken888686@gmail.com>
2022-03-11 18:07:28 +0800

    pipe-1

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,5 +1,13 @@
-import { BadRequestException, Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpStatus,
+  NotAcceptableException,
+  Param,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ParseIntPipe } from '../../pipes/parse-int.pipe';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +22,21 @@ export class AuthController {
   @Get('/error')
   getError() {
     throw new BadRequestException('Auth Error');
+  }
+
+  @Get('/check/:id')
+  checkId(
+    @Param(
+      'id',
+      // new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+      // new ParseIntPipe({
+      //   exceptionFactory: () => new NotAcceptableException('id需為數字'),
+      // }),
+      ParseIntPipe,
+    )
+    id: number,
+  ) {
+    console.log(id);
+    return this.authService.getAuth();
   }
 }
